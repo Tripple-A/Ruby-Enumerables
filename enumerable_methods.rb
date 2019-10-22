5ca3c473bf91067b8ddcb5301f3a948087157f3f
Biodun <biodun9@gmail.com>
2019-10-22 20:41:01 +0100

    added my_any method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -46,14 +46,23 @@ module Enumerable
     test
   end
 
-  
+  def my_any? (classification=nil)
+    test = false
+    my_each { |item| return true unless yield(item) == false } if block_given?
+    my_each {|item| return true unless item.class==classification} if classification.class==Class
+    my_each { |item| return true unless item =~ classification } if classification.class == Regexp
+    my_each { |item| return true unless item == classification } if [Integer, String].include?(classification.class)
+    my_each { |item| return true if item } if !classification && !block_given?
+    test
+  end
 end
 
 arr = [4, 2, 1, 8, 4, 1, 0, 8, 5, 7, 5, 5, 4, 5, 3, 4, 4, 4, 7, 8, 5, 2, 6, 8, 4, 4, 1, 7, 6, 8, 5, 4, 6, 1, 5, 8, 2, 7, 1, 8, 1, 1, 0, 4, 5, 7, 0, 3, 4, 3, 6, 3, 1, 4, 2, 3, 8, 8, 5, 4, 0, 8, 1, 6, 7, 2, 6, 4, 6, 1, 6, 4, 7, 6, 0, 7, 8, 4, 0, 2, 3, 3, 6, 0, 1, 1, 5, 4, 6, 1, 7, 7, 8, 5, 8, 0, 4, 1, 7, 5]
 true_array = [1, true, 'hi', nil]
 words = %w[dog door rod blade]
 
-print true_array.my_all?
-print arr.my_all?(Integer)
-print words.my_all?(/d/)
-print arr.my_all?(3)
+print arr.my_any?
+print arr.my_any?(Integer)
+print words.my_any?(/z/)
+words[0] = 'cat'
+words.my_any?('cat')
