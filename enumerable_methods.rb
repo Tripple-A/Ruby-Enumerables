ad04f9bc8497772254e7464239492eda914c858a
Biodun <biodun9@gmail.com>
2019-10-22 20:48:25 +0100

    added my_none method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -55,14 +55,36 @@ module Enumerable
     my_each { |item| return true if item } if !classification && !block_given?
     test
   end
+
+  def my_none? (classification=nil)
+    test = true
+    my_each { |item| return false if yield(item) == true } if block_given?
+    my_each {|item| return false if item.class==classification} if classification.class==Class
+    my_each { |item| return false if item =~ classification } if classification.class == Regexp
+    my_each { |item| return false if item == classification } if [Integer, String].include?(classification.class)
+    my_each { |item| return false if item } if !classification && !block_given?
+    test
+  end
+
+  def my_count
+    return length unless block_given?
+
+    counter = 0
+    (0...length).each do |i|
+      counter += 1 if yield(self[i]) == true
+    end
+    counter
+  end
+
 end
 
 arr = [4, 2, 1, 8, 4, 1, 0, 8, 5, 7, 5, 5, 4, 5, 3, 4, 4, 4, 7, 8, 5, 2, 6, 8, 4, 4, 1, 7, 6, 8, 5, 4, 6, 1, 5, 8, 2, 7, 1, 8, 1, 1, 0, 4, 5, 7, 0, 3, 4, 3, 6, 3, 1, 4, 2, 3, 8, 8, 5, 4, 0, 8, 1, 6, 7, 2, 6, 4, 6, 1, 6, 4, 7, 6, 0, 7, 8, 4, 0, 2, 3, 3, 6, 0, 1, 1, 5, 4, 6, 1, 7, 7, 8, 5, 8, 0, 4, 1, 7, 5]
-true_array = [1, true, 'hi', nil]
+false_arr = [nil, false, nil, false]
+
 words = %w[dog door rod blade]
 
-print arr.my_any?
-print arr.my_any?(Integer)
-print words.my_any?(/z/)
+print false_arr.my_none?
+print arr.my_none?(Integer)
+print words.my_none?(/z/)
 words[0] = 'cat'
-words.my_any?('cat')
+print words.my_none?('cat')
