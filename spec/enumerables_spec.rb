3c432d5b32134c010dea58f88cda7762d01436a9
Biodun <biodun9@gmail.com>
2019-10-31 18:59:35 +0100

    added the my_each test

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
 require './enumerable_methods.rb'
+
+describe "Enumerable" do
+    describe "#my_each" do
+     it "iterates the given block for each array item" do
+           
+    end
+    
+end
