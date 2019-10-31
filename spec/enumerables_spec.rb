ad0994cc78d975728202affa5cc6b749e7a1e6a0
abruzy <dialloabubakar5861@gmail.com>
2019-10-31 19:15:59 +0100

    added rspec test for my_each method

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -2,10 +2,19 @@
 
 require './enumerable_methods.rb'
 
-describe "Enumerable" do
-    describe "#my_each" do
-     it "iterates the given block for each array item" do
-           
+describe 'Enumerable' do
+  let(:num_arr) { [3, 4, 2, 1] }
+  let(:empty_arr) { [] }
+
+  describe '#my_each' do
+    context 'If block is given' do
+      it 'iterates the given block for each array item' do
+        my_array = []
+        original_array = []
+        num_arr.my_each { |i| my_array << i + 2 }
+        num_arr.each { |i| original_array << i + 2 }
+        expect(my_array).to eql(original_array)
+      end
     end
-    
+  end
 end
